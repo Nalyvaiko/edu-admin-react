55b1b1de26b1ea82bbc0af363e7902086668865e
Vitalii Nalyvaiko <v.yu.nalyvaiko@gmail.com>
2017-01-30 15:04:10 +0200

    fix build script for heroku

--- tools/distServer.js
+++ tools/distServer.js
@@ -1,26 +1,25 @@
 import express from 'express';
+import path from 'path';
+import open from 'open';
 import compression from 'compression';
 
 /* eslint-disable no-console */
 
-// If running from Heroku - use env.port, else 3000
-const PORT = process.env.PORT || 3000;
+const port = 3000;
 const app = express();
 
 app.use(compression());
-
-//express middleware, redirect traffic from https to http
-app.use(function(req, res, next) {
-    if (req.headers['x-forwarded-proto'] === 'https') {
-        res.redirect('http://' + req.hostname + req.url);
-    } else {
-        next();
-    }
-});
-
-// which folder to serve
 app.use(express.static('dist'));
 
-app.listen(PORT, function() {
-    console.log('Express server is up on port ' + PORT);
-})
+app.get('*', function(req, res) {
+    res.sendFile(path.join( __dirname, '../dist/index.html'));
+});
+
+app.listen(port, function(err) {
+    if (err) {
+        console.error(err);
+    } else {
+        console.log(`==> Running on http://localhost:${port}`);
+        // open(`http://localhost:${port}`);
+    }
+});
